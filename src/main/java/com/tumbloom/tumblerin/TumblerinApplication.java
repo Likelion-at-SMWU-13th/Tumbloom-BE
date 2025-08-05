22610fd9897d9c2ef2da62c5ffbd2e7ab70be4a3
SangHee-Hong <hongdabagi@gmail.com>
2025-08-05 12:35:32 +0900

    chore: 프포젝트 초기 세팅

---
+++ src/main/java/com/tumbloom/tumblerin/TumblerinApplication.java
@@ -0,0 +1,13 @@
+package com.tumbloom.tumblerin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumblerinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumblerinApplication.class, args);
+	}
+
+}
