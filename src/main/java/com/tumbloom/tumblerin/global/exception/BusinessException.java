59191656d6d1b99d0f77cda476dff815b8e9b67d
SangHee-Hong <hongdabagi@gmail.com>
2025-08-07 16:56:15 +0900

    Chore : BusinessException 관련하여 응답 형식 구조 개선

--- src/main/java/com/tumbloom/tumblerin/global/exception/BusinessException.java
+++ src/main/java/com/tumbloom/tumblerin/global/exception/BusinessException.java
@@ -6,14 +6,17 @@ import lombok.Getter;
 @Getter
 public class BusinessException extends RuntimeException {
     private final ErrorCode errorCode;
+    private final String detail;
 
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.detail = null;
     }
 
-    public BusinessException(ErrorCode errorCode, String detailMessage) {
-        super(detailMessage);
+    public BusinessException(ErrorCode errorCode, String detail) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
+        this.detail = detail;
     }
 }
