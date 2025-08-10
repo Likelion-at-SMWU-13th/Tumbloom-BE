a4e2de22d6e8f29dec8b283c6e9de2bfdfc81700
hyhy-j <lhjlhj10088@gmail.com>
2025-08-11 01:02:59 +0900

    Feat : CafeDetailResponseDTO 작성

--- src/main/java/com/tumbloom/tumblerin/app/dto/Cafedto/CafeDetailResponseDTO.java
+++ src/main/java/com/tumbloom/tumblerin/app/dto/Cafedto/CafeDetailResponseDTO.java
@@ -3,8 +3,26 @@ package com.tumbloom.tumblerin.app.dto.Cafedto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
 public class CafeDetailResponseDTO {
+
+    private Long id;
+    private String cafeName;
+    private String address;
+    private String imageUrl;
+    private String businessHours;
+    private String callNumber;
+    private List<MenuDTO> menuList;
+
+    @Getter
+    @AllArgsConstructor
+    public static class MenuDTO {
+        private Long id;
+        private String menuName;
+        private Integer price;
+    }
 
 }
