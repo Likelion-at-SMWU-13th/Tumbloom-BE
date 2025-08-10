3cbf4dd60883f6b08b13e729a15f4913507fc9d8
hyhy-j <lhjlhj10088@gmail.com>
2025-08-11 01:44:37 +0900

    Feat : Favorite Entity 구현

---
+++ src/main/java/com/tumbloom/tumblerin/app/domain/Favorite.java
@@ -0,0 +1,33 @@
+package com.tumbloom.tumblerin.app.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Cafe cafe;
+
+    private LocalDateTime createdAt;
+
+}
