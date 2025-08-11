85646bad65d6222248bc1545f7bf72514089f474
SangHee-Hong <hongdabagi@gmail.com>
2025-08-12 01:53:51 +0900

    Feat: UserPreference entity 설계

--- src/main/java/com/tumbloom/tumblerin/app/domain/UserPreference.java
+++ src/main/java/com/tumbloom/tumblerin/app/domain/UserPreference.java
@@ -4,11 +4,17 @@ import com.tumbloom.tumblerin.app.domain.Preference.ExtraOption;
 import com.tumbloom.tumblerin.app.domain.Preference.PreferredMenu;
 import com.tumbloom.tumblerin.app.domain.Preference.VisitPurpose;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Table(name = "user_preference")
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserPreference {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
