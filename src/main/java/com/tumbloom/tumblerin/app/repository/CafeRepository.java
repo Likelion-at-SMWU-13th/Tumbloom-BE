e3ba7d0286c51b9610df01650732fcfb8e871cd0
hyhy-j <lhjlhj10088@gmail.com>
2025-08-11 14:59:28 +0900

    Add : 카페 검색 쿼리 추가

--- src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
+++ src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
@@ -2,7 +2,10 @@ package com.tumbloom.tumblerin.app.repository;
 
 import com.tumbloom.tumblerin.app.domain.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
@@ -10,5 +13,12 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
     Optional<Cafe> findByCafeName(String cafeName);
     Optional<Cafe> findByQrLink(String qrLink);
     Optional<Cafe> findByVerificationCode(String verificationCode);
+
+    @Query("""
+        SELECT c FROM Cafe c
+        WHERE REPLACE(LOWER(c.cafeName), ' ', '') LIKE CONCAT('%', LOWER(REPLACE(:keyword, ' ', '')), '%')
+            OR REPLACE(LOWER(c.address), ' ', '') LIKE CONCAT('%', LOWER(REPLACE(:keyword, ' ', '')), '%')
+""")
+    List<Cafe> searchByCafeNameOrAddress(@Param("keyword") String keyword);
 
 }
