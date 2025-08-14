4ab7b6b65fb5eeecae5075855ac1abde607205b9
SangHee-Hong <hongdabagi@gmail.com>
2025-08-14 22:27:50 +0900

    Refactor: ai 추천 카페에 스크랩 여부 필드 포함

--- src/main/java/com/tumbloom/tumblerin/app/service/CafeRecommendationService.java
+++ src/main/java/com/tumbloom/tumblerin/app/service/CafeRecommendationService.java
@@ -5,6 +5,7 @@ import com.tumbloom.tumblerin.app.domain.User;
 import com.tumbloom.tumblerin.app.domain.UserPreference;
 import com.tumbloom.tumblerin.app.dto.Cafedto.CafeRecommendDTO;
 import com.tumbloom.tumblerin.app.repository.CafeRepository;
+import com.tumbloom.tumblerin.app.repository.FavoriteRepository;
 import com.tumbloom.tumblerin.app.repository.UserPreferenceRepository;
 import com.tumbloom.tumblerin.global.dto.ErrorCode;
 import com.tumbloom.tumblerin.global.exception.BusinessException;
@@ -23,6 +24,7 @@ public class CafeRecommendationService {
     private final CafeRepository cafeRepository;
     private final UserPreferenceRepository upRepository;
     private final OpenAIEmbeddingService embeddingService;
+    private final FavoriteRepository favoriteRepository;
 
     public List<CafeRecommendDTO> recommendCafesForUser(Long userId) {
 
@@ -33,7 +35,7 @@ public class CafeRecommendationService {
             return null;
         }
 
-        //1. 사용자 취향 → 텍스트 변환
+        // 1. 사용자 취향 → 텍스트 변환
         String userText = Stream.of(
                         List.of("방문목적: " + String.join(", ", convertEnumToStringList(preference.getVisitPurposes()))),
                         List.of("선호메뉴: " + String.join(", ", convertEnumToStringList(preference.getPreferredMenus()))),
@@ -42,18 +44,23 @@ public class CafeRecommendationService {
                 .flatMap(List::stream)
                 .collect(Collectors.joining("; "));
 
-        // 2️. 사용자 텍스트 임베딩
+        // 2. 사용자 텍스트 임베딩
         List<Double> userEmbedding = embeddingService.createEmbeddingList(userText);
 
-        // 3️. DB에 있는 카페 모두 조회
+        // 3. DB에 있는 카페 모두 조회
         List<Cafe> cafes = cafeRepository.findAll();
 
-        // 4️. 코사인 유사도 계산
+        // 3-1. 사용자 즐겨찾기 카페 ID 목록
+        List<Long> favoriteCafeIds = favoriteRepository.findCafeIdsByUserId(userId);
+
+        // 4. 코사인 유사도 계산
         return cafes.stream()
                 .map(cafe -> {
                     List<Double> cafeEmbedding = embeddingService.jsonToVector(cafe.getEmbedding());
                     double similarity = cosineSimilarity(userEmbedding, cafeEmbedding);
-                    return new CafeRecommendDTO(cafe, similarity);
+                    boolean isFavorite = favoriteCafeIds.contains(cafe.getId());
+
+                    return new CafeRecommendDTO(cafe, similarity, isFavorite); //
                 })
                 .sorted((a, b) -> Double.compare(b.getSimilarity(), a.getSimilarity()))
                 .limit(5) // 상위 5개만 추천
