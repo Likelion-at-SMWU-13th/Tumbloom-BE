05292270bdc72f120829cbae58925fe2b2e3524c
SangHee-Hong <hongdabagi@gmail.com>
2025-08-16 02:49:35 +0900

    Feat: 마이페이지 DTO에 사용자 취향(topPreferences)와 레벨 진행도(levelProgress)필드 추가

--- src/main/java/com/tumbloom/tumblerin/app/dto/Userdto/UserMyPageResponseDTO.java
+++ src/main/java/com/tumbloom/tumblerin/app/dto/Userdto/UserMyPageResponseDTO.java
@@ -2,15 +2,23 @@ package com.tumbloom.tumblerin.app.dto.Userdto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Getter
+@Setter
 @Builder
 public class UserMyPageResponseDTO {
     private String nickname;
-    private int level;
+    private String level;
     private int remainingSteps;
     private int tumblerUsageCount;
     private int couponIssuedCount;
     private int availableCoupons;
     private int favoriteCafeCount;
+
+    private List<String> topPreferences;
+    private double levelProgress;
+
 }
