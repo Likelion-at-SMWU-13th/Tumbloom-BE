cd67302f9626a42a421d48ea9458ce89afa98064
hyhy-j <lhjlhj10088@gmail.com>
2025-08-16 16:00:52 +0900

    Add : 3km 이내 카페 리스트, top5 쿼리 작성

--- src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
+++ src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
@@ -1,6 +1,7 @@
 package com.tumbloom.tumblerin.app.repository;
 
 import com.tumbloom.tumblerin.app.domain.Cafe;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,11 +15,36 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
     Optional<Cafe> findByQrLink(String qrLink);
     Optional<Cafe> findByVerificationCode(String verificationCode);
 
+    // 카페 검색
     @Query("""
         SELECT c FROM Cafe c
         WHERE REPLACE(LOWER(c.cafeName), ' ', '') LIKE CONCAT('%', LOWER(REPLACE(:keyword, ' ', '')), '%')
             OR REPLACE(LOWER(c.address), ' ', '') LIKE CONCAT('%', LOWER(REPLACE(:keyword, ' ', '')), '%')
 """)
     List<Cafe> searchByCafeNameOrAddress(@Param("keyword") String keyword);
+
+    // 3km 이내 카페 리스트
+    @Query("""
+        SELECT c FROM Cafe c
+        WHERE function('ST_Distance_Sphere',
+                        c.location,
+                        function('ST_SRID', function('POINT', :lon, :lat), 4326)) <= :radiusMeters
+        ORDER BY c.id ASC
+        """)
+    List<Cafe> findNearbyCafeList(@Param("lon") double lon, @Param("lat") double lat, @Param("radiusMeters") double radiusMeters);
+
+    // top5
+    @Query("""
+        SELECT c
+        FROM Cafe c
+        WHERE function('ST_Distance_Sphere',
+                       c.location,
+                       function('ST_SRID', function('POINT', :lon, :lat), 4326)) <= :radiusMeters
+        ORDER BY function('ST_Distance_Sphere',
+                          c.location,
+                          function('ST_SRID', function('POINT', :lon, :lat), 4326)) ASC,
+                  c.id ASC
+        """)
+    List<Cafe> findTopByDistance(@Param("lon") double lon, @Param("lat") double lat, @Param("radiusMeters") double radiusMeters, Pageable pageable);
 
 }
