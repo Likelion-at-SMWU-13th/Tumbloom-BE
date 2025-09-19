4ae4f5dcae8bd5bc52402e65f252313878b7c822
hyhy-j <lhjlhj10088@gmail.com>
2025-09-19 13:28:27 +0900

    Fix : 거리 기반 카페 탐색 쿼리 수정

--- src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
+++ src/main/java/com/tumbloom/tumblerin/app/repository/CafeRepository.java
@@ -24,27 +24,34 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
     // 3km 이내 카페 리스트
     @Query(value = """
+    WITH p AS (
+        SELECT ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography AS u
+    )
     SELECT c.*
-    FROM cafe c
-    WHERE ST_Distance_Sphere(
-            c.location,
-            ST_SRID(POINT(:lon, :lat), 4326)
-          ) <= :radiusMeters
-    ORDER BY ST_Distance_Sphere(
-            c.location,
-            ST_SRID(POINT(:lon, :lat), 4326)
-          )
+    FROM cafe c, p
+    WHERE ST_DWithin(c.location, p.u, :radiusMeters)
+    ORDER BY ST_Distance(c.location, p.u)
     """, nativeQuery = true)
     List<Cafe> findNearbyCafeList(@Param("lon") double lon, @Param("lat") double lat, @Param("radiusMeters") double radiusMeters);
 
     // top5
     @Query(value = """
-    SELECT c.*
-    FROM cafe c
-    ORDER BY ST_Distance_Sphere(
-            c.location,
-            ST_SRID(POINT(:lon, :lat), 4326)
-          )
+    WITH p AS (
+    SELECT
+      ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)            AS u_geom,
+      ST_SetSRID(ST_MakePoint(:lon, :lat), 4326)::geography AS u_geog
+    ),
+    knn AS (
+    SELECT c.id
+    FROM cafe c, p
+    ORDER BY c.location_geom <-> p.u_geom 
+    LIMIT 50                            
+    )
+    SELECT c.*,
+         ST_Distance(c.location, p.u_geog) AS dist_m       
+    FROM knn k
+    JOIN cafe c ON c.id = k.id, p
+    ORDER BY ST_Distance(c.location, p.u_geog)               
     LIMIT 5
     """, nativeQuery = true)
     List<Cafe> findNearbyTop5CafeList(@Param("lon") double lon, @Param("lat") double lat);
